6d6c04d37d8e851a2463c84409cae70e45f2344b
Ander00333 <mercadomiguelangel132@gmail.com>
2026-04-18 10:13:17 -0500

    la mayoria esta listo

---
+++ BuenasPracticas/src/buenaspracticas/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package buenaspracticas;
+
+/**
+ *
+ * @author User
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] datos = {10, 20, 30, 40};
+
+        double promedio = Operaciones.calcularPromedio(datos);
+
+        System.out.println("El promedio es: " + promedio);
+    }
+}
---
+++ BuenasPracticas/src/buenaspracticas/Operaciones.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package buenaspracticas;
+
+/**
+ *
+ * @author User
+ */
+public class Operaciones {
+
+    /**
+     * Calcula el promedio de un arreglo de números
+     */
+    public static double calcularPromedio(int[] numeros) {
+
+        
+    }
+}
